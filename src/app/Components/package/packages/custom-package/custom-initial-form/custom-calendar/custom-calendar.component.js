2f8313882aaabfd93f27b000361844a773b9182a
Jack Mennie <jack.mennie@live.com>
2017-07-28 13:04:15 +1000

    started working more on the calendar to add functionality. Attempting to work out how to set a selected day and current day for CSS

--- src/app/Components/package/packages/custom-package/custom-initial-form/custom-calendar/custom-calendar.component.js
+++ src/app/Components/package/packages/custom-package/custom-initial-form/custom-calendar/custom-calendar.component.js
@@ -13,7 +13,66 @@ var CustomCalendarComponent = (function () {
         this.days = ['25', '26', '27', '28', '29', '30', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
             '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23',
             '24', '25', '26', '27', '28', '29', '30', '31', '01', '02', '03', '04', '05'];
+        this.month = "July";
+        this.selectedDay = 26; //day that is selected.
+        this.currentDay = 28; //current day
+        this.calendarday = 'normal';
     }
+    CustomCalendarComponent.prototype.openMonth = function () {
+        this.selected = 1;
+    };
+    CustomCalendarComponent.prototype.openYear = function () {
+        this.selected = 2;
+    };
+    CustomCalendarComponent.prototype.close = function () {
+        this.selected = 0;
+    };
+    /* sets the month value of the date */
+    CustomCalendarComponent.prototype.updateMonth = function (value) {
+        switch (value) {
+            case 0:
+                this.month = "January";
+                break;
+            case 1:
+                this.month = "February";
+                break;
+            case 2:
+                this.month = "March";
+                break;
+            case 3:
+                this.month = "April";
+                break;
+            case 4:
+                this.month = "May";
+                break;
+            case 5:
+                this.month = "June";
+                break;
+            case 6:
+                this.month = "July";
+                break;
+            case 7:
+                this.month = "August";
+                break;
+            case 8:
+                this.month = "September";
+                break;
+            case 9:
+                this.month = "October";
+                break;
+            case 10:
+                this.month = "November";
+                break;
+            case 11:
+                this.month = "December";
+                break;
+        }
+        this.selected = 0;
+    };
+    /* updates the selected day */
+    CustomCalendarComponent.prototype.setDay = function (value) {
+        this.selectedDay = value;
+    };
     return CustomCalendarComponent;
 }());
 CustomCalendarComponent = __decorate([
@@ -21,6 +80,7 @@ CustomCalendarComponent = __decorate([
         moduleId: module.id,
         selector: 'custom-calendar',
         templateUrl: 'custom-calendar.component.html',
+        styleUrls: ['custom-calendar.component.css']
     })
 ], CustomCalendarComponent);
 exports.CustomCalendarComponent = CustomCalendarComponent;
--- src/app/Components/package/packages/custom-package/custom-initial-form/custom-calendar/custom-calendar.component.ts
+++ src/app/Components/package/packages/custom-package/custom-initial-form/custom-calendar/custom-calendar.component.ts
@@ -4,6 +4,7 @@ import {Component} from '@angular/core'
     moduleId: module.id,
     selector: 'custom-calendar',
     templateUrl: 'custom-calendar.component.html',
+    styleUrls: ['custom-calendar.component.css']
 })
 
 export class CustomCalendarComponent {
@@ -11,5 +12,48 @@ export class CustomCalendarComponent {
     
     days : string[] = ['25','26','27','28','29','30','01','02','03','04','05','06','07','08','09','10',
             '11','12','13','14','15','16','17','18','19','20','21','22','23',
-            '24','25','26','27','28','29','30','31','01','02','03','04','05']
+            '24','25','26','27','28','29','30','31','01','02','03','04','05'];
+
+    month : string = "July";
+    selectedDay : number = 26;  //day that is selected.
+    currentDay  : number = 28;  //current day
+
+    calendarday = 'normal';
+
+
+    openMonth() {
+        this.selected = 1;
+    }
+
+    openYear() {
+        this.selected = 2;
+    }
+
+    close() {
+        this.selected = 0;
+    }
+
+    /* sets the month value of the date */
+    updateMonth(value : number) {
+        switch(value) {
+            case 0: this.month = "January"; break;
+            case 1: this.month = "February"; break;
+            case 2: this.month = "March"; break;
+            case 3: this.month = "April"; break;
+            case 4: this.month = "May"; break;
+            case 5: this.month = "June"; break;
+            case 6: this.month = "July"; break;
+            case 7: this.month = "August"; break;
+            case 8: this.month = "September"; break;
+            case 9: this.month = "October"; break;
+            case 10: this.month = "November"; break;
+            case 11: this.month = "December"; break;
+        }
+        this.selected = 0;
+    }
+
+    /* updates the selected day */
+    setDay(value : number) {
+        this.selectedDay = value;
+    }
 }
