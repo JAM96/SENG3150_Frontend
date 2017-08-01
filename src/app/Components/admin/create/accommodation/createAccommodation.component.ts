4bc14b3abdc55d1d5d3ebef3b801a190eba326bd
Legend9119 <nicholaskhendry@gmail.com>
2017-08-01 21:44:03 +1000

    Added very basic form to create a new Accommodation item in the Admin Panel.

--- src/app/Components/admin/create/accommodation/createAccommodation.component.ts
+++ src/app/Components/admin/create/accommodation/createAccommodation.component.ts
@@ -8,24 +8,19 @@ import {Router} from '@angular/router'
 })
 
 export class CreateAccommodationComponent{
-    // //This is the number which represents the page that the user wants to go to next.
-    // //Set to 0 (ie. back to the home page), by default.
-    // selectedOption:number = 0;
+    //This is the number which represents the page that the user wants to go to next.
+    //Set to 0 (ie. back to the admin panel starting page), by default.
+    selectedOption:number = 0;
 
-    // constructor(
-    //     public router: Router
-    // ){}
+    constructor(
+        public router: Router
+    ){}
 
-    // setNavOption(selection: number){
-    //     this.selectedOption = selection;
+    setNavOption(selection: number){
+        this.selectedOption = selection;
 
-    //     switch(this.selectedOption){
-    //         case 0: this.router.navigate(['/']); break;
-    //         case 1: this.router.navigate(['/createAccommodation']); break;
-    //         case 2: this.router.navigate(['/createPackage']); break;
-    //         case 3: this.router.navigate(['/createActivity']); break;
-    //         case 4: this.router.navigate(['/createEvent']); break;
-    //         case 5: this.router.navigate(['/createFoodAndDrink']); break;
-    //     }
-    // }
+        switch(this.selectedOption){
+            case 0: this.router.navigate(['/admin']); break;
+        }
+    }
 }
