af22de419f1add4f16124caf5e2613cbcc577105
Jack Mennie <jack.mennie@live.com>
2017-08-19 15:41:46 +1000

    Fixed start error

--- src/app/Objects/FoodAndDrinks/MockData/mock-food-and-drinks.js
+++ src/app/Objects/FoodAndDrinks/MockData/mock-food-and-drinks.js
@@ -5,46 +5,19 @@ exports.FOODANDDRINKS_LIST = [
         foodAndDrinksID: "a23923",
         name: "Meet",
         starRating: 5,
+        stars: [1, 1, 1, 1, 1],
         address: "2 Honeysuckle Dr, Newcastle New South Wales 2300, Australia",
         suburb: "Honeysuckle",
         menuTheme: "Everything",
         websiteURL: "http://meet.restaurant/",
         briefDescription: "Barbeque with a Brazillian twist",
         expenseRating: 3,
+        expense: [0, 0, 0],
         diningStyle: "Restauraunt",
         phoneNo: '042142303',
         photoURL: 'food.jpg',
-        userRating: 420
-    },
-    {
-        foodAndDrinksID: "a23923",
-        name: "Masdfsdfasdfet",
-        starRating: 5,
-        address: "2 Honeysuckle Dr, Newcastle New South Wales 2300, Australia",
-        suburb: "Honeysuckle",
-        menuTheme: "Everything",
-        websiteURL: "http://meet.restaurant/",
-        briefDescription: "Barbeque with a Brazillian twist",
-        expenseRating: 3,
-        diningStyle: "Restauraunt",
-        phoneNo: '042142303',
-        photoURL: 'food.jpg',
-        userRating: 420
-    },
-    {
-        foodAndDrinksID: "a23923",
-        name: "Meeasdfsadft",
-        starRating: 5,
-        address: "2 Honeysuckle Dr, Newcastle New South Wales 2300, Australia",
-        suburb: "Honeysuckle",
-        menuTheme: "Everything",
-        websiteURL: "http://meet.restaurant/",
-        briefDescription: "Barbeque with a Brazillian twist",
-        expenseRating: 3,
-        diningStyle: "Restauraunt",
-        phoneNo: '042142303',
-        photoURL: 'food.jpg',
-        userRating: 420
+        userRating: 420,
+        rating: 'Good'
     }
 ];
 //# sourceMappingURL=mock-food-and-drinks.js.map
